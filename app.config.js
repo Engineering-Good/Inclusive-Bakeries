2251512b052f824710025c312781099b162293b7
Kenny Chow <kenny@chow.my>
2025-10-23 08:54:10 +0800

    Updated Lefu config and app version

--- app.config.js
+++ app.config.js
@@ -10,8 +10,8 @@ export default ({ config }) => {
         ...baseConfig.extra?.eas, // Also spread existing EAS properties if they exist
         projectId: "2b2885a1-106c-4edc-ab00-74af333f62c0", // Ensure your actual project ID is here
       },
-      LEFU_API_KEY: process.env.LEFU_API_KEY ?? "default_api_key",
-      LEFU_API_SECRET: process.env.LEFU_API_SECRET ?? "default_api_secret",
+      LEFU_API_KEY: process.env.LEFU_API_KEY ?? "lefuad83f4414473e85e",
+      LEFU_API_SECRET: process.env.LEFU_API_SECRET ?? "Q1dZvqOt6Lb48q6OhFr6Sf6JS9IyEQLFjrNwYuMyipM=",
     },
   };
 };

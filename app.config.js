a19870710c9a4d6f46c51b7e9321fe20f4dad966
jaden <jaden.tohjm@gmail.com>
2026-04-17 13:40:08 +0800

    Update GettingStarted.md with wsl2 guide and improvement. Remove API key and secret. Remove some unneed package during expo doctor check.

--- app.config.js
+++ app.config.js
@@ -8,10 +8,9 @@ export default ({ config }) => {
       ...baseConfig.extra, // <<< THIS IS THE CRUCIAL LINE! Spread existing extra properties
       eas: {
         ...baseConfig.extra?.eas, // Also spread existing EAS properties if they exist
-        projectId: "2b2885a1-106c-4edc-ab00-74af333f62c0", // Ensure your actual project ID is here
       },
-      LEFU_API_KEY: process.env.LEFU_API_KEY ?? "lefuad83f4414473e85e",
-      LEFU_API_SECRET: process.env.LEFU_API_SECRET ?? "Q1dZvqOt6Lb48q6OhFr6Sf6JS9IyEQLFjrNwYuMyipM=",
+      LEFU_API_KEY: process.env.LEFU_API_KEY,
+      LEFU_API_SECRET: process.env.LEFU_API_SECRET,
     },
   };
 };
